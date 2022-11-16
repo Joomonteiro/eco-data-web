29ce615d3f03cc9614b5591ab10a1e05a19e289b
João Pedro Monteiro Pereira <joao.pedro@helpper.com.br>
2022-11-15 21:29:14 -0300

    fix: next js config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   // webpackDevMiddleware: config => {
   //   config.watchOptions = {
   //     poll: 1000,
